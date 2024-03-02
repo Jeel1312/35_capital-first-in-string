c3822a6a06e6b19d8ff0710afae5cee5dd13e9f6
Jeel1312 <jeelpatel.13121999@gmail.com>
2024-03-02 19:40:50 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ function cap() {
 
     let a = document.getElementById('a').value;
 
-    let b = a.charAt(0  ).toUpperCase() + a.slice(1);
+    let b = a.charAt(0).toUpperCase() + a.slice(1);
 
     document.getElementById("Capitalize").innerHTML ="Capitalized String : " + b;
 }
